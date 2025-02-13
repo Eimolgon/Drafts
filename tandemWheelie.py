c437418c6ceab837ae312b87cf602c72ff4bbb6b
Eimolgon <eimol.bgt@gmail.com>
2025-02-13 18:27:49 +0100

    Update tandemwheelie with real values

--- tandemWheelie.py
+++ tandemWheelie.py
@@ -6,22 +6,22 @@ g = 9.81
 lb2kg = 0.453592
 in2m = 0.0254
 
-m1 = 70                 # Rider 1 mass
-m2 = 80                 # Rider 2 mass
-b1 = 0.6                # Rider 1 CoM longitudinal position / 0.9 original
-b2 = 0.0                # Rider 2 CoM longitudinal position / 0.3 original
-h1 = 0.75                # Rider 1 CoM height / 0.9 original
-h2 = 0.75                # Rider 2 CoM height
+m1 = 72                 # Rider 1 mass
+m2 = 72                 # Rider 2 mass
+b1 = 1                  # Rider 1 CoM longitudinal position / 0.9 original
+b2 = 0.4                # Rider 2 CoM longitudinal position / 0.3 original
+h1 = 0.8                # Rider 1 CoM height / 0.9 original
+h2 = 0.8                # Rider 2 CoM height
 
-me = 20                  # Extra mass / 0 original
-be = -0.15              # Extra mass longitudinal position
-he = ((3/4)*29) * in2m        # Extra mass height / 29 inch original
+me = 30                 # Extra mass / 0 original
+be = -0.2               # Extra mass longitudinal position
+he = 0.9                # Extra mass height / 29 inch original
 
-mb = 40 * lb2kg         # lb to kg
+mb = 12 + 17            # Bicycle mass
 mt = m1 + m2 + mb + me  # Total mass
-w = 68.5 * in2m         # inch to m
+w = 1.9                 # Wheelbase
 
-Rr = (26 * in2m)/2      # Rear wheel radius
+Rr = 0.34               # Rear wheel radius
 mux = 0.8               # Rear wheel friction coefficient
 Rdr = 208.8/1000        # Rear drivetrain radius 
 Rdf = 144.2/1000        # Front drivetrain radius 34T (https://www.wolftoothcomponents.com/pages/chainring-diameter-by-tooth-count)
@@ -42,6 +42,10 @@ Xrg = ((w - b)/(w - mux * h))* mux * mt * g
 Fc = Tr/Rdr
 Fp = (Fc * Rdf)/Rc
 
+# Balance angle
+
+alpha = (np.arctan(h/b))*180/np.pi
+
 print('System CoM: %2.2f %2.2f (b, h) [m] \n' % (b, h))
 
 print('Total mass: %2.2f [kg] \n' % (mt))
@@ -53,5 +57,7 @@ print('Maximum allowable rear wheel force for grip: %2.2f [N]' % (Xrg))
 print('Force difference: %2.2f [N] \n' % (Xrg - Xr))
 
 print('Chain force: {0:2.2f} [N]'.format(Fc))
-print('Pedal force (each rider): %2.2f [N] = %2.2f [kg]' % (Fp/2, (Fp/2)/g))
+print('Pedal force (each rider): %2.2f [N] = %2.2f [kg] \n' % (Fp/2, (Fp/2)/g))
+
+print('Pitch angle for balance: %2.2f °' % (90 - alpha))
 
