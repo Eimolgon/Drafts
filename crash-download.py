ad37e84853d81e48e09323a0beef0310a5c7c5c7
Eimolgon <eimol.bgt@gmail.com>
2025-04-14 17:03:53 +0200

    Update dataset

--- crash-download.py
+++ crash-download.py
@@ -9,7 +9,8 @@ End time.
 Cookies file is in Netscape HTTP format.
 '''
 
-file = '/mnt/c/Users/begtgonzalez/Onedrive - Delft University of Technology/Documents/University/Data/dataset-yt4.csv'
+#file = '/mnt/c/Users/begtgonzalez/Onedrive - Delft University of Technology/Documents/University/Data/dataset-yt4.csv'
+file = '/mnt/c/Users/begtgonzalez/Onedrive - Delft University of Technology/Documents/University/Bicycle-Crashes/Data/02-Video-labelling/dataset-nocrash.csv'
 cookie_path = '/mnt/c/Users/begtgonzalez/Onedrive - Delft University of Technology/Documents/University/Data/Cookies2.txt'
 
 
@@ -27,7 +28,7 @@ with open(file, newline = '') as csvfile:
         try:
             yt_opts = {
                 'verbose': True,
-                'outtmpl': f'/mnt/c/Users/begtgonzalez/Onedrive - Delft University of Technology/Documents/University/Bicycle-Crashes/Video/Dataset/{idx}',
+                'outtmpl': f'/mnt/c/Users/begtgonzalez/Onedrive - Delft University of Technology/Documents/University/Bicycle-Crashes/Video/Dataset-NoCrash/yt-nocrash-{idx}',
                 'download_ranges': download_range_func(None, [(start_time, end_time)]),
                 'force_keyframes_at_cuts': True,
                 'format_sort': ['res:1080', 'ext:mp4:m4a'],
