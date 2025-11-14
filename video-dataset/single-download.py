2a9764d71d233435ba1bf8897284184d7dd73912
Eimolgon <eimol.bgt@gmail.com>
2025-11-14 17:05:02 +0100

    update classification guide

--- video-dataset/single-download.py
+++ video-dataset/single-download.py
@@ -1,6 +1,8 @@
 import yt_dlp
 from yt_dlp.utils import download_range_func
 
+cookie_path = r'{PATH_TO_COOKIES}/cookies.txt'
+
 url = str(input('Paste your URL here: '))
 start_time = int(input('Paste yout start time (in seconds): '))
 end_time = int(input('Paste yout end time (in seconds): '))
@@ -14,6 +16,7 @@ yt_opts = {
                                                        end_time)]),
         'force_keyframes_at_cuts': True,
         'format_sort': ['res:1080', 'ext:mp4:m4a'],
+        'cookiefile': cookie_path
         }
 
 with yt_dlp.YoutubeDL(yt_opts) as ydl:
